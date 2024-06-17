0ca6d5f9ab37454d2e3dbdcff485cbd49efaaab8
GERMETKER <leha.usanov.0808@mail.ru>
2024-06-17 16:50:12 +0500

    Добавьте файлы проекта.

---
+++ DZ12/DZ12.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <Windows.h>
+const int size = 4;
+void FirstFunk();
+void SecFunk();
+void ThirFunk();
+void ForthFunk();
+void (*FunkARR[size])();
+
+void main()
+{
+	FunkARR[0] = FirstFunk;
+	FunkARR[1] = SecFunk;
+	FunkARR[2] = ThirFunk;
+	FunkARR[3] = ForthFunk;
+	for (int i = 0; i < size; i++)
+	{
+		(*FunkARR[i])();
+	}
+}
+void FirstFunk()
+{
+	std::cout << "One\n";
+}
+void SecFunk()
+{
+	std::cout << "Two\n";
+}
+void ThirFunk()
+{
+	std::cout << "Three\n";
+}
+void ForthFunk()
+{
+	std::cout << "Four\n";
+}
